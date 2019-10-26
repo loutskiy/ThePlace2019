32b4d1980e2b8d2b37de13159cc7e8c045a11792
Михаил Луцкий <mikhail@lwts.ru>
2019-10-26 11:21:19 +0200

    mapcontroller

---
+++ ThePlace2019/MapController.swift
@@ -0,0 +1,34 @@
+//
+//  MapController.swift
+//  ThePlace2019
+//
+//  Created by Михаил Луцкий on 26.10.2019.
+//  Copyright © 2019 Mikhail Lutskii. All rights reserved.
+//
+
+import UIKit
+
+class MapController: UIView {
+
+    let kCONTENT_XIB_NAME = "MapController"
+    @IBOutlet var contentView: UIView!
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        Bundle.main.loadNibNamed(kCONTENT_XIB_NAME, owner: self, options: nil)
+        contentView.fixInView(self)
+        contentView.layer.cornerRadius = 4.0
+        contentView.layer.borderWidth = 1.0
+//        contentView.layer.borderColor = UIColor.carCheckViewBorderColor.cgColor
+//        contentView.backgroundColor = .carCheckViewColor
+    }
+}
---
+++ ThePlace2019/UIView+extension.swift
@@ -0,0 +1,22 @@
+//
+//  UIView+extension.swift
+//  ThePlace2019
+//
+//  Created by Михаил Луцкий on 26.10.2019.
+//  Copyright © 2019 Mikhail Lutskii. All rights reserved.
+//
+
+import Foundation
+
+extension UIView
+{
+    func fixInView(_ container: UIView!) -> Void{
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
