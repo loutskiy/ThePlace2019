c7ca3be4b184bd15fe0a4bc17aef1f9520917f64
Михаил Луцкий <mikhail@lwts.ru>
2019-10-26 11:16:10 +0200

    first

---
+++ ThePlace2019/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ThePlace2019
+//
+//  Created by Михаил Луцкий on 26.10.2019.
+//  Copyright © 2019 Mikhail Lutskii. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
