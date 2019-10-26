77d3c3e27f3aa84614d219eb89fde8e37affe907
Михаил Луцкий <mikhail@lwts.ru>
2019-10-26 11:42:26 +0200

    heat map google

---
+++ ThePlace2019/Header-Bridge.h
@@ -0,0 +1,14 @@
+//
+//  Header-Bridge.h
+//  ThePlace2019
+//
+//  Created by Михаил Луцкий on 26.10.2019.
+//  Copyright © 2019 Mikhail Lutskii. All rights reserved.
+//
+
+#ifndef Header_Bridge_h
+#define Header_Bridge_h
+
+#import "GMUMarkerClustering.h"
+
+#endif /* Header_Bridge_h */
